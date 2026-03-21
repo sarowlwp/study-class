746fad8b8736d2d2c43deed385a12585d605cb8c
lwp <lwp@zhihu.com>
2026-03-21 23:56:05 +0800

    fix(raz): remove unnecessary typing_extensions, use Python 3.10+ stdlib typing

--- app/services/speech_assessment.py
+++ app/services/speech_assessment.py
@@ -1,13 +1,7 @@
 import os
 import random
-import sys
 from dataclasses import dataclass, field
-from typing import List
-
-if sys.version_info >= (3, 8):
-    from typing import Protocol, runtime_checkable
-else:
-    from typing_extensions import Protocol, runtime_checkable
+from typing import List, Protocol, runtime_checkable
 
 
 @dataclass
